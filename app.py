aaaaa29635e957caa4852afeef99d2ed55b36f73
Ahammadbi123 <ahammadbishaik986@gmail.com>
2026-06-11 21:22:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def predict():
 
 if __name__ == '__main__':
     # Using debug=True helps in development to see errors instantly
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
